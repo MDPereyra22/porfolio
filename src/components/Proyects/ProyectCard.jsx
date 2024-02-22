eeca6ee51dc83d6b8e86b4dd41a4b7a6357e49d1
MDPereyra22 <pereyra.dmatias@gmail.com>
2024-02-21 22:29:31 -0300

    Limpieza linea en ProyectCard

--- src/components/Proyects/ProyectCard.jsx
+++ src/components/Proyects/ProyectCard.jsx
@@ -8,13 +8,12 @@ const ProjectCard = ({ screenshot, title, description, link }) => (
     animate={{ opacity: 1, scale: 1 }}
     transition={{ duration: 0.5 }}
   >
-    <div className="relative overflow-hidden rounded-lg mb-4">
+    <div className="relative overflow-hidden rounded-xl mb-4">
       <img
         src={screenshot}
         alt={`Screenshot de ${title}`}
         className="w-full h-48 object-cover transition-transform transform hover:scale-105"
       />
-      <div className="absolute inset-0 bg-black opacity-30 transition-opacity hover:opacity-20"></div>
     </div>
     <h3 className="text-xl font-semibold mb-2 text-white">{title}</h3>
     <p className="text-gray-300 mb-4">{description}</p>
